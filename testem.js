31bf2bb99e83c288f653a32dd67a517dab3b6a5f
Devonte <devo.tox.89@gmail.com>
2017-12-19 16:02:54 +0000

    fix disconnects

--- testem.js
+++ testem.js
@@ -1,19 +1,21 @@
 /* eslint-env node */
 module.exports = {
-  test_page: 'tests/index.html?hidepassed',
-  disable_watching: true,
-  launch_in_ci: [
-    'Chrome'
-  ],
-  launch_in_dev: [
-    'Chrome'
-  ],
-  browser_args: {
-    Chrome: [
-      '--disable-gpu',
-      '--headless',
-      '--remote-debugging-port=9222',
-      '--window-size=1440,900'
-    ]
-  }
+	test_page: 'tests/index.html?hidepassed',
+	disable_watching: true,
+	launch_in_ci: [
+		'Chrome'
+	],
+	launch_in_dev: [
+		'Chrome'
+	],
+	browser_start_timeout: 60,
+	browser_disconnect_timeout: 30,
+	browser_args: {
+		Chrome: [
+			'--headless',
+			'--disable-gpu',
+			'--window-size=1440,900',
+			'--remote-debugging-port=9222'
+		]
+	}
 };
