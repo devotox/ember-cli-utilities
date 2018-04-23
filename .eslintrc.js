6a3a563934befb828c92c51a665fc6047e62d6f7
Devonte <devo.tox.89@gmail.com>
2018-04-23 16:54:55 +0100

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,25 @@ module.exports = {
     browser: true
   },
   rules: {
+    'no-var': 'error',
+    'no-console': 'off',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'indent': ['error', 'tab'],
+    'max-len': ['error', 140 ],
+    'no-extra-parens': 'error',
+    // 'prettier/prettier': 'error',
+    'comma-dangle': ['error', 'never'],
+    'arrow-parens': ['error', 'always'],
+    'no-cond-assign': ['error', 'always'],
+    'no-template-curly-in-string': 'error',
+    'object-shorthand': ['error', 'always'],
+    'object-curly-spacing': ['error', 'always'],
+    'max-statements-per-line': ['error', { 'max': 2 }],
+    'new-cap': ['error', { 'capIsNewExceptions': ['A'] }],
+    'no-constant-condition': ['error', { 'checkLoops': false }],
+    'brace-style': ['error', 'stroustrup', { 'allowSingleLine': true }],
+    'generator-star-spacing': ['error', { 'before': false, 'after': true }]
   },
   overrides: [
     // node files
