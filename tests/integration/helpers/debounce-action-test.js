d625edee1973dc14b5cde4268f7255ae24ea051c
Devonte <devo.tox.89@gmail.com>
2019-03-19 14:05:54 +0000

    fix tests

--- tests/integration/helpers/debounce-action-test.js
+++ tests/integration/helpers/debounce-action-test.js
@@ -15,7 +15,7 @@ module('Integration | Helper | debounce-action', function(hooks) {
 		const returnedFunction = `function(event) {
 			if (isRunning) { return; }
 
-			later(() => isRunning = false, debounceTime);
+			Ember.run.later(() => isRunning = false, debounceTime);
 			isRunning = !isRunning;
 			return action(event);
 		}`.replace(/\s/g, '');
