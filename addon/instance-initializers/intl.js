d5518eb1974795031546e7de402416aaba7b327d
Devonte <devo.tox.89@gmail.com>
2019-06-21 11:03:27 +0100

    fix intl instance initializer

--- addon/instance-initializers/intl.js
+++ addon/instance-initializers/intl.js
@@ -7,10 +7,9 @@ export function initialize(application) {
 	let userLanguage = (device.query('lang') || navigator.language).toLowerCase();
 
 	let currentLanguage = allLanguages.includes(userLanguage) 
-		? userLanguage : intl.get('locale')[0]
+		? userLanguage : intl.get('locale')[0];
 
 	intl.set('locale', currentLanguage);
-	intl.set('primaryLocale', currentLanguage);
 
 	console.info('[intl]', 'User Language:', userLanguage); // eslint-disable-line
 	console.info('[intl]', 'Current Language:', currentLanguage); // eslint-disable-line
@@ -19,6 +18,5 @@ export function initialize(application) {
 
 export default {
 	name: 'intl',
-	after: 'ember-intl',
 	initialize
 };
