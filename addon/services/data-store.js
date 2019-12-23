d820d78faeeacd343d0904922ad4b81423acbfd4
Devonte <devo.tox.89@gmail.com>
2019-12-23 15:30:23 +0100

    add jsbeautifyrc

--- addon/services/data-store.js
+++ addon/services/data-store.js
@@ -4,9 +4,36 @@ export default class DataStore extends Store {
 	query(queryOrExpression, options = { reload: true }, id = undefined) {
 		return super.query(queryOrExpression, options, id);
 	}
-	findRecordByAttribute(type, attribute, value, options) {
+	findRecordsByAttribute(type, attribute, value, options) {
 		return this.query((q) => {
 			return q.findRecords(type).filter({ attribute, value });
 		}, options);
 	}
+	async findRecordByAttribute(type, attribute, value, options) {
+		const record = await this.findRecordsByAttribute(type, attribute, value, options);
+		return record[0];
+	}
 }
+
+
+// findRecordsByAttribute(
+// 	type: string,
+// 	attribute: string,
+// 	value: string,
+// 	options?: object
+// ): Promise<Model[]> {
+// 	return this.query((q) => {
+// 		return q.findRecords(type).filter({ attribute, value });
+// 	}, options);
+// }
+
+// async findRecordByAttribute(
+// 	type: string,
+// 	attribute: string,
+// 	value: string,
+// 	options?: object
+// ): Promise<Model> {
+// 	const record = await this.findRecordsByAttribute(type, attribute, value, options);
+
+// 	return record[0];
+// }
