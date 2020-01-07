5e9260b8363fbeaf52d272dd1d4e91c9b90303b6
Devonte <devo.tox.89@gmail.com>
2020-01-07 16:25:31 +0000

    Fix no intl installed

--- addon/instance-initializers/intl.js
+++ addon/instance-initializers/intl.js
@@ -3,13 +3,13 @@ export function initialize(application) {
 	let intl = application.lookup('service:intl');
 	let device = application.lookup('service:device');
 
-	let allLanguages = intl.get('locales');
+	let allLanguages = intl?.get('locales') || [];
 	let userLanguage = (device.query('lang') || navigator.language || '').toLowerCase();
 
 	let currentLanguage = allLanguages.includes(userLanguage) 
-		? userLanguage : intl.get('locale')[0];
+		? userLanguage : intl?.get('locale')[0] || '';
 
-	intl.set('locale', currentLanguage);
+	intl?.set('locale', currentLanguage);
 
 	console.info('[intl]', 'User Language:', userLanguage); // eslint-disable-line
 	console.info('[intl]', 'Current Language:', currentLanguage); // eslint-disable-line
